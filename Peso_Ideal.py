68e7020a3401596bd20087c415ad7b70d67d0734
Danizinh <danimaltoc@gmai.com>
2022-03-22 00:56:39 -0300

    frist-commit

--- Peso_Ideal.py
+++ Peso_Ideal.py
@@ -1,12 +1,12 @@
-def peso_Ideal(altura, sexo):
-            if sexo == "F":
-                print(f"Seu PESO IDEAL É { (62.1 * altura) - 44.7:.1f} KG")
-            elif sexo == "M":
-                 print(f"Seu PESO IDEAL É {(72.7 * altura) - 58:.1f} KG")
+def peso_Ideal(altura, sex):
+            if sex == "F":
+                print(f"Your ideal weight is ? { (62.1 * altura) - 44.7:.1f} KG")
+            elif sex == "M":
+                 print(f"Your ideal weight is ? {(72.7 * altura) - 58:.1f} KG")
             else:
-                print("Erro! O PROGRAMA SERA FECHADO")
+                print("Error! THE PROGRAM WILL BE CLOSED")
 
 
-Altura =float(input("Informe sua Altura:"))
-Sex = str(input("SEXO [F/M]:")).strip()
+Altura =float(input("Enter your height:"))
+Sex = str(input("SEX [F/M]:")).strip()
 peso_Ideal(Altura,Sex)
